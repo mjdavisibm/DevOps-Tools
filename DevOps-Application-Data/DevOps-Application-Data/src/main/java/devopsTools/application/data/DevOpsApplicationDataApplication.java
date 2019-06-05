7490989b71e36c3fbc79990be2baf2888b12f3a0
Matt J Davis <mjdavis@us.ibm.com>
2019-06-04 20:02:14 -0600

    Initial Commit

---
+++ DevOps-Application-Data/DevOps-Application-Data/src/main/java/devopsTools/application/data/DevOpsApplicationDataApplication.java
@@ -0,0 +1,13 @@
+package devopsTools.application.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplicationDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplicationDataApplication.class, args);
+	}
+
+}
